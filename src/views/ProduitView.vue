e11ac98be3deb1dd6c863474fd265eca9adf4044
Amelie Lock-Fat <amelie.lock-fat@etud.univ-jfc.fr>
2023-02-06 08:47:40 +0000

    Update 1

---
+++ src/views/ProduitView.vue
@@ -0,0 +1,37 @@
+<template>
+    <main>
+         <div>
+            <table>
+                <caption>Liste des produits</caption>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prix</th>
+                    <th>Stock</th>
+                    <th>Commandés</th>
+                </tr>
+            </table>
+        </div>
+    </main>
+ </template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+td,
+th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+
+th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #232623;
+    color: rgb(255, 255, 255);
+}
+</style>
